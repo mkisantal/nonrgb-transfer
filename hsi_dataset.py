1d2d8fb0ec60c0c6ac2718d641ea05723de3f7f3
mkisantal <kisantal.mate@gmail.com>
2019-03-11 15:40:25 +0100

    splitting dataset with multiview scenes

--- hsi_dataset.py
+++ hsi_dataset.py
@@ -68,10 +68,10 @@ def npy_hsi_loader(chs, path, normalize=True):
     return torch.tensor(image)
 
 
-def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', per_category_image_limit=None,
-                  semi_supervised=False):
+def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', classification=True,
+                  image_limit=None, semi_supervised=False):
 
-    """ Splitting dataset to train (val) and test sets. Optionally converting to .npy for faster loading. """
+    """ Splitting dataset to train (val) and test sets. Assumes that samples are in subfolders by classes. """
 
     dataset_name = os.path.basename(os.path.normpath(root_dir))
 
@@ -79,7 +79,7 @@ def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', pe
     split /= split.sum()
 
     if split.shape[0] == 2:
-        split_names = ['train', 'test']
+        split_names = ['train', 'val']
     elif split.shape[0] == 3:
         split_names = ['train', 'val', 'test']
     else:
@@ -87,16 +87,80 @@ def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', pe
 
     if semi_supervised:
         split_names.append('minitrain')
-        if per_category_image_limit is None:
+        if image_limit is None:
             raise ValueError('You need to specify \'per_category_image_limit\' to create semi-supervised dataset.')
 
     # create root folders for dataset partitions
     split_roots = []
     for name in split_names:
         split_root = os.path.join(root_dir, '..', '{}_{}{}'.format(dataset_name, dataset_suffix, name))
-        os.makedirs(split_root)
+        if not os.path.exists(split_root):
+            os.makedirs(split_root)
         split_roots.append(split_root)
 
+    if classification:
+        split_classification(root_dir, split, split_roots, semi_supervised, image_limit, convert)
+    else:
+        split_segmentation(root_dir, split, split_roots, semi_supervised, image_limit, convert)
+
+    return
+
+
+def split_segmentation(root_dir, split, split_roots, semi_supervised, image_limit, convert):
+
+    """ Splitting the dataset to train, val and test sets. Making sure scenes are separated! """
+
+    image_names = os.listdir(root_dir)
+    shuffle(image_names)
+
+    multiview_dict = {}
+    for v, image_name in enumerate(image_names):
+        view_code = image_name[:7]
+        if view_code not in multiview_dict.keys():
+            multiview_dict[view_code] = []
+        multiview_dict[view_code].append(image_name)
+
+    scenes = list(multiview_dict.keys())
+    shuffle(scenes)
+
+    source_image_paths = []
+    destination_image_paths = []
+
+    num_images_in_split = [int(x * len(image_names)) for x in split]
+    print('{}: {}'.format('Estimated number of images in splits: ', num_images_in_split))
+    split_limits = np.append([0], np.cumsum(num_images_in_split))
+    split_limits[-1] = len(image_names)
+
+    view_counter, scene_counter = 0, -1
+    for i in range(split_limits.shape[0] - 1):
+        scene_counter += 1  # always move multiview pointer when new subset started
+        if len(scenes) == scene_counter:
+            break
+        view_counter = 0
+        for j in range(split_limits[i], split_limits[i + 1]):
+            if len(multiview_dict[scenes[scene_counter]]) == view_counter:
+                scene_counter += 1
+                view_counter = 0
+                if len(scenes) == scene_counter:
+                    break
+            view = multiview_dict[scenes[scene_counter]][view_counter]
+            source_image_paths.append(os.path.join(root_dir, view))
+            destination_image_paths.append(os.path.join(split_roots[i], view))
+            view_counter += 1
+            if semi_supervised and view_counter < image_limit:
+                source_image_paths.append(os.path.join(root_dir, view))
+                destination_image_paths.append(os.path.join(split_roots[-1], view))
+            if image_limit is not None and j - split_limits[i] == image_limit - 1:
+                break
+
+    src_dst_dicts = [{'src': src, 'dst': dst} for src, dst in zip(source_image_paths, destination_image_paths)]
+    with Pool(8) as p:
+        for _ in tqdm.tqdm(p.imap(copy_image, src_dst_dicts), total=len(src_dst_dicts)):
+            pass
+    return
+
+
+def split_classification(root_dir, split, split_roots, semi_supervised, per_category_image_limit, convert):
     # loop over classes
     category_folders = os.listdir(root_dir)
     for category in category_folders:
@@ -122,11 +186,11 @@ def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', pe
 
         # divide images to partitions
         destination_image_paths = []
-        num_images_in_split = [int(x*len(image_names)) for x in split]
+        num_images_in_split = [int(x * len(image_names)) for x in split]
         print('{}: {}'.format('Number of images in splits:', num_images_in_split))
         split_limits = np.append([0], np.cumsum(num_images_in_split))
         split_limits[-1] = len(image_names)
-        for i in range(split_limits.shape[0]-1):
+        for i in range(split_limits.shape[0] - 1):
             for j in range(split_limits[i], split_limits[i + 1]):
                 destination_image_paths.append(os.path.join(split_category_paths[i], image_names[j]))
                 if semi_supervised and i == 0 and j < per_category_image_limit:
@@ -143,7 +207,14 @@ def split_dataset(root_dir, split=[.8, .2], convert=False, dataset_suffix='', pe
             for src, dst in zip(source_image_paths, destination_image_paths):
                 copyfile(src, dst)
         print('\n\n')
+    return
 
+
+def copy_image(src_dst_dict):
+
+    """ Simple function for parallelization. """
+
+    copyfile(src_dst_dict['src'], src_dst_dict['dst'])
     return
 
 
@@ -193,9 +264,11 @@ if __name__ == '__main__':
 
     # tests
     # split_dataset(r"C:\datasets\EuroSATallBands", split=[.8, .2], convert=True, dataset_suffix='npy_')
-    split_dataset("/home/mate/dataset/EuroSATallBands", split=[.8, .2], convert=True, dataset_suffix='semi_',
-                  per_category_image_limit=300, semi_supervised=True)
+    # split_dataset("/home/mate/dataset/EuroSATallBands", split=[.8, .2], convert=True, dataset_suffix='semi_',
+    #               per_category_image_limit=300, semi_supervised=True)
     # a = hsi_loader([1], r'C:\datasets\EuroSATallBands_train\Residential\Residential_1006.tif')
+    # split_dataset(root_dir="C:\datasets\grss\Track1-MSI-1", classification=False, image_limit=None)
+    split_dataset(root_dir='/home/mate/datasets/Track1-MSI', classification=False, image_limit=None)
     print('done.')
 
 
