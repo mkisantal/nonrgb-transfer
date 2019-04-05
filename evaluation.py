cb05be4dee0a1567e711fe51f6e72d5373223d2b
mkisantal <kisantal.mate@gmail.com>
2019-04-05 15:47:12 +0200

    generating transformed images for segmentation

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
-from hsi_dataset import load_single_image
+from hsi_dataset import load_single_image, HsiSegmentationDataset
 import torch
+from albumentations import Compose, CenterCrop
 
 
 def to_color_image(torch_tensor, pil=True):
@@ -25,6 +26,8 @@ def inspect_images(model, device, num_samples=5, channels=None, gen_only=False):
 
     """ Visualizing learned HSI Image embedding. """
 
+    # TODO: adapt this for new dataset
+    # TODO: calculate whole batch at once?
     model.eval()
     rows = []
     for image_id in range(num_samples):
@@ -41,6 +44,37 @@ def inspect_images(model, device, num_samples=5, channels=None, gen_only=False):
                 else:
                     transformed_image = model.module.get_transformed_input(x)
             converted_images.append(to_color_image(transformed_image, pil=False))
+        appended_rgb = np.hstack(rgb_images)
+        appended_conv = np.hstack(converted_images)
+        row = np.vstack([appended_rgb, appended_conv])
+        rows.append(row)
+    single_image = np.vstack(rows)
+    return Image.fromarray(single_image)
+
+
+def inspect_images2(model, device, num_samples=2, channels=None, gen_only=False):
+
+    """ Visualizing learned segmentation MSI image embedding. """
+
+    model.eval()
+    rows = []
+
+    transforms = Compose([CenterCrop(224, 224)])
+    dataset = HsiSegmentationDataset(root='/home/mate/datasets/grss/Track1-MSI_A_val',
+                                     gt_root='/home/mate/datasets/grss/Train-Track1-Truth',
+                                     channels=None, transforms=transforms)
+
+    for index in range(num_samples):
+        rgb_images = []
+        converted_images = []
+        for category_id in range(num_samples * 2):
+            with torch.set_grad_enabled(False):
+                img_npy, gt_tensor = dataset[index + 10 * category_id]
+                image_in = torch.tensor(img_npy).unsqueeze(0).to(device)
+                out, transformed = model(image_in)
+
+                rgb_images.append(to_color_image(image_in, pil=False))
+                converted_images.append(to_color_image(transformed, pil=False))
         appended_rgb = np.hstack(rgb_images)
         appended_conv = np.hstack(converted_images)
         row = np.vstack([appended_rgb, appended_conv])
